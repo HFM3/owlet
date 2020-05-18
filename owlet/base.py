2a97ddc24199ce937574952e7a101fbf1238f7d5
HFM3 <24279380+HFM3@users.noreply.github.com>
2020-05-17 22:11:40 -0400

    Delete base.py

--- owlet/base.py
+++
@@ -1,9 +0,0 @@
-import owlet
-
-x = owlet.read_egf("/Users/henryiii/Documents/GitHub/owlet/excluded/egf_examples/PostOfficeSquare.egf")
-
-# owlet.visualize(x)
-my_shape = owlet.read_csv("/Users/henryiii/Documents/GitHub/owlet/excluded/PostOfficeSquare_Boundary.csv")
-
-owlet.write_kml(path, my_shape, 'Park Name', 'Parks', altitude_mode='rtg')
-# owlet.write_kml("/Users/henryiii/Documents/GitHub/owlet/excluded/PostOfficeSquare_Boundary.kml", x, 'Park Name', 'Parks')
